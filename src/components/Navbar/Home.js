5498610270092050ebeb41d64599a9e50736be30
Tony Reynolds <treyno@gmail.com>
2024-10-10 22:24:59 +0100

    now display users on homepage

--- src/components/Navbar/Home.js
+++ src/components/Navbar/Home.js
@@ -1,24 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { getUsers } from './Users';
 
+const UserList = ({ users }) => {
+  return (
+    <div>
+      <h2>User List</h2>
+      <ul style={{ listStyleType: 'none', padding: 0 }}>
+        {users.map(user => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 function Home() {
-  return <h2>Home Page</h2>;
-  
+  const [users, setUsers] = useState([]);
 
+  useEffect(() => {
+    async function fetchUsers() {
+      try {
+        const fetchedUsers = await getUsers();
+        setUsers(fetchedUsers);
+      } catch (error) {
+        console.error("Error fetching users:", error);
+      }
+    }
+    fetchUsers();
+  }, []);
+
+  return (
+    <div>
+      <h2>Home Page</h2>
+      <UserList users={users} />
+    </div>
+  );
 }
-
-async function displayUsers() {
-  try {
-    const users = await getUsers();
-    users.forEach(user => {
-      console.log(`User ${user.id}: ${user.name}`);
-    });
-  } catch (error) {
-    console.error("Error displaying users:", error);
-  }
-}
-
-displayUsers();
 
 export default Home;
